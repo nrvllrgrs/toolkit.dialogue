52798b784d9c5cec3c3185ea225032cd286f62d6
TITAN\ckilg <ckilgore@gmail.com>
2025-04-18 07:52:26 -0500

    Added smart evaluators that adjust max based on number of items in list

---
+++ Runtime/Scoring/Evaluators/DialogueSmartCategoyPriorityEvaluator.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace ToolkitEngine.Dialogue.Scoring
+{
+	[EvaluableCategory("Dialogue")]
+	public class DialogueSmartCategoryPriorityEvaluator : BaseEvaluator
+	{
+		#region Properties
+
+		public float max => DialogueManager.CastInstance.Config.categories.Length;
+
+		#endregion
+
+		#region Methods
+
+		protected override float CalculateNormalizedScore(GameObject actor, GameObject target, Vector3 position)
+		{
+			if (actor.TryGetComponent(out DialogueRunnerControl control)
+				&& DialogueManager.CastInstance.TryGetDialogueCategory(control.dialogueType, out var category))
+			{
+				int priority = DialogueManager.CastInstance.GetCategoryPriority(category);
+				if (priority > 0)
+				{
+					return MathUtil.GetPercent(priority, 0f, max);
+				}
+			}
+			return 0f;
+		}
+
+		#endregion
+	}
+}
---
+++ Runtime/Scoring/Evaluators/DialogueSmartPriorityEvaluator.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ToolkitEngine.Dialogue.Scoring
+{
+	[EvaluableCategory("Dialogue")]
+	public class DialogueSmartPriorityEvaluator : BaseEvaluator
+	{
+		#region Methods
+
+		protected override float CalculateNormalizedScore(GameObject actor, GameObject target, Vector3 position)
+		{
+			if (actor.TryGetComponent(out DialogueRunnerControl control))
+			{
+				int priority = DialogueManager.CastInstance.GetPriority(control.dialogueType);
+				if (priority > 0)
+				{
+					return MathUtil.GetPercent(priority, 0f, GetMax(control.dialogueType));
+				}
+			}
+			return 0f;
+		}
+
+		private float GetMax(DialogueType dialogueType)
+		{
+			return DialogueManager.CastInstance.TryGetDialogueCategory(dialogueType, out var category)
+				? category.priorities.Length
+				: 0f;
+		}
+
+		#endregion
+	}
+}
